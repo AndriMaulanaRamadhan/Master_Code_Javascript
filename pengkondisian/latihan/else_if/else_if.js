41dd137d709b461030aec9ad604bab6bb5591697
yura <andrimaulanaramadhan54@gmail.com>
2022-02-08 19:30:50 +0700

    try

--- pengkondisian/latihan/else_if/else_if.js
+++ pengkondisian/latihan/else_if/else_if.js
@@ -4,9 +4,7 @@ var angkotBeroperasi = 6;
 for (noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
   if (noAngkot <= angkotBeroperasi) {
     console.log ('Angkot no. ' + noAngkot + 'beroperasi dengan baik')
-  } else if (noAngkot === 8) {
-    console.log ('Angkot no. ' + noAngkot + 'sedang lembur')
-  } else if (noAngkot === 10) {
+  } else if (noAngkot === 8 || noAngkot === 10) {
     console.log ('Angkot no. ' + noAngkot + 'sedang lembur')
   } else {
     console.log ('Angkot no. ' + noAngkot + 'tidak beroperasi dengan baik')
