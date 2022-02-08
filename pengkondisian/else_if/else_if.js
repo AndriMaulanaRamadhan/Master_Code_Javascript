f30be3f242b8807db74c92420c9d23337e35aeb6
yura <andrimaulanaramadhan54@gmail.com>
2022-02-08 19:20:57 +0700

    pengkondisian

---
+++ pengkondisian/else_if/else_if.js
@@ -0,0 +1,9 @@
+var angka = prompt ('Masukkan nilai angka');
+
+if (angka % 2 == 0) {
+  alert ('Angka memiliki nilai Genap');
+} else if (angka % 2 == 1) {
+  alert ('Angka memiliki nilai Ganjil');
+} else {
+  alert ('Yang anda masukkan bukanlah angka');
+}
---
+++ pengkondisian/latihan/else_if/else_if.js
@@ -0,0 +1,13 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+
+for (noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
+  if (noAngkot <= angkotBeroperasi) {
+    console.log ('Angkot no. ' + noAngkot + 'beroperasi dengan baik')
+  } else if (noAngkot === 8) {
+    console.log ('Angkot no. ' + noAngkot + 'sedang lembur')
+  }
+   else {
+    console.log ('Angkot no. ' + noAngkot + 'tidak beroperasi dengan baik')
+  }
+}
