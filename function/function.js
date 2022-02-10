b9dc80d5d5638eceedd1c336802720539c7301dc
yura <andrimaulanaramadhan54@gmail.com>
2022-02-10 19:42:26 +0700

    try

--- function/function.js
+++ function/function.js
@@ -13,3 +13,20 @@ function rumusVolumeKubus (a,b) {
 
 alert (rumusVolumeKubus(8,3));
 alert (rumusVolumeKubus(15,20));
+
+function tambah (a,b) {
+  var totalTambah;
+  totalTambah = a + b;
+
+  return totalTambah;
+}
+
+function kali (a,b) {
+  var totalKali;
+  totalKali = a * b;
+
+  return totalKali;
+}
+
+var jumlahToko = kali (tambah (1,2), tambah (3,4));
+alert (jumlahToko);
