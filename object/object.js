7d15a84373b568f6b3c73664a4889f9df9738ee3
yura <andrimaulanaramadhan54@gmail.com>
2022-02-12 22:23:51 +0700

    object

---
+++ object/object.js
@@ -0,0 +1,10 @@
+var mhs = {
+  nama = 'Andri Maulana Ramadhan',
+  umur = 21,
+  ips = [2.50,3.00,3.89],
+  alamat = {
+    jalan = 'jl.abc No.87',
+    kota = 'karawang',
+    provinsi = 'jawa barat'
+  }
+};
